8d49128bab7bc13afd31c9cf256f11a4a2605bd5
moriol <moriol@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2009-07-18 16:23:28 +0000

    Did not add the exception class previously created.
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@28 7f4fa506-1231-44ca-96bd-ce53b71a5133

---
+++ yeti/YetiCallException.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package yeti;
+
+/**
+ * Class that represents an exception or an error thrown during a call.
+ * 
+ * @author Manuel Oriol (manuel@cs.york.ac.uk)
+ * @date Jul 18, 2009
+ *
+ */
+@SuppressWarnings("serial")
+public class YetiCallException extends Exception {
+	
+	public String log;
+	public Throwable originalThrowable;
+	
+	
+	/**
+	 * Simple getter for the logs.
+	 * 
+	 * @return the logs for the throwable.
+	 */
+	public String getLog() {
+		return log;
+	}
+
+	/**
+	 * Simple getter for the Throwable
+	 * 
+	 * @return the Throwable originally thrown.
+	 */
+	public Throwable getOriginalThrowable() {
+		return originalThrowable;
+	}
+
+
+	/**
+	 * Simple constructor for the Exception.
+	 * 
+	 * @param log the log at the moment of the error.
+	 * @param originalThrowable the original Throwable.
+	 */
+	public YetiCallException(String log, Throwable originalThrowable) {
+		super();
+		this.log = log;
+		this.originalThrowable = originalThrowable;
+	}
+	
+	
+	
+
+}
