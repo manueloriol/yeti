5e7fd881a096b011b7d2b5c93692fc6cba6e3a54
faheem <faheem@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2009-08-19 20:54:41 +0000

    Yeti is no longer called from within the main method here, instead the MapJob is used for this purpose.
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@66 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/YetiJob.java
+++ yeti/YetiJob.java
@@ -2,44 +2,33 @@
 package yeti;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.filecache.DistributedCache;
 
-import yeti.Yeti;
 
 public class YetiJob {
 	
 	public static String dfsInput;
 	public static String dfsOutput;
+	
+	
+	public static void main(String[] args) {
 
-public static void main(String[] args) {
-
-		Yeti.YetiRun(args);
-				
-		
 		JobClient client = new JobClient();
 		JobConf conf = new JobConf(yeti.YetiJob.class);
 		
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(IntWritable.class);
 		
-		FileInputFormat.setInputPaths(conf, new Path (dfsInput));
-		FileOutputFormat.setOutputPath(conf, new Path(dfsOutput));
-
+		FileInputFormat.setInputPaths(conf, new Path (args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
 		
 		conf.setMapperClass(yeti.YetiMap.class);
 		conf.setReducerClass(yeti.YetiReducer.class);
-
+		
 		client.setConf(conf);
 		try {
 			JobClient.runJob(conf);
