5cdf44a1467c43f5498173af509bea570906a6fa
faheem <faheem@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2009-08-19 20:52:50 +0000

    Yeti Map now reads the command line arguments from a file in the Input directory on DFS specified as command line argument when calling YetiJob.
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@65 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/YetiMap.java
+++ yeti/YetiMap.java
@@ -2,23 +2,27 @@ package yeti;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
-import yeti.Yeti;
-import yeti.YetiLogProcessor;
 
 public class YetiMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>  {
 
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 
-		String line = value.toString();
-		Text out = new Text("MyResult");
-		output.collect(out, new IntWritable(Integer.parseInt(line)));
+		String rawParams = value.toString();
+		String params [] = rawParams.split("\\x20");
+				
+		Yeti.YetiRun(params);
+		YetiLogProcessor lp = (YetiLogProcessor)Yeti.pl.getLogProcessor();
 		
+		Text out = new Text("MyResult");
+		output.collect(out, new IntWritable(lp.listOfErrors.size()));
 		
 	}
 }
