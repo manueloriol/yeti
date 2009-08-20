0e554a50ac9e8b23fa83d40eecf972cfbbe3fab6
faheem <faheem@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2009-08-20 22:14:02 +0000

    Added Comments
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@72 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/YetiReducer.java
+++ yeti/YetiReducer.java
@@ -9,17 +9,27 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
+/**
+ * Class that represents the Reducer for Yeti to run on cloud.
+ * 
+ * @author Faheem Ullah (fu500@cs.york.ac.uk)
+ * @date August 20, 2009
+ */
+
 public class YetiReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
 	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 		
 		int total = 0;
-		
+		//we sum all the intermediate values (i.e. number of bugs found by each Mapjob for yeti)
 		while (values.hasNext())
 			total += values.next().get();
-		
-		output.collect(new Text("MyValueTOtal"), new IntWritable (total));
+
+		//This is the sum of all the bugs found by running each line of arguments, 
+		//that is why the Bugs would be non-unique
+		output.collect(new Text("Non-Unique Bugs"), new IntWritable(total));
+
+//		output.collect(new Text("Unique Errors"), new IntWritable (YetiJob.uniquelistOfErrors.size()));
 		
 	}
-
 }
