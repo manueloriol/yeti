a4880c156b374a39f1e83e091f518e0c2c5c0050
faheem <faheem@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2009-08-20 22:13:10 +0000

    Added comments where necessary.
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@70 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/YetiMap.java
+++ yeti/YetiMap.java
@@ -1,6 +1,9 @@
 package yeti;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Date;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -10,18 +13,39 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
+/**
+ * Class that represents the Maper for Yeti to run on cloud.
+ * 
+ * @author Faheem Ullah (fu500@cs.york.ac.uk)
+ * @date August 20, 2009
+ */
 
 public class YetiMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>  {
+	
 
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 
-		String rawParams = value.toString();
-		String params [] = rawParams.split("\\x20");
-				
+		//spliting the lines into parameters based on whitespaces
+		String params [] = value.toString().split("\\x20");
+		
+		//passing parameters (Command line arguments) to Yeti
 		Yeti.YetiRun(params);
+		
 		YetiLogProcessor lp = (YetiLogProcessor)Yeti.pl.getLogProcessor();
 		
-		Text out = new Text("MyResult");
+//		Collection<String> col= lp.listOfErrors.keySet();
+//		Iterator<String> itr = col.iterator();
+//				
+//		while(itr.hasNext()){
+//			String yetiKey = itr.next();
+//			System.out.println(yetiKey);
+//			if (!YetiJob.uniquelistOfErrors.containsKey(yetiKey)) {
+//				System.out.println("placed in unique list of Errors: "+yetiKey);
+//				YetiJob.uniquelistOfErrors.put(lp.listOfErrors.get(yetiKey).toString(), new Date());
+//			}
+//		}
+		
+		Text out = new Text("Non-Unique Bugs");
 		output.collect(out, new IntWritable(lp.listOfErrors.size()));
 		
 	}
