f9b9a2ca1b442e0afe2e2adf67c24c0a87f29970
dong <dong@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2010-10-05 16:39:36 +0000

    make the execution of the command line in a child thread, so that the main testing thread would not be blocked when the command line is blocked. the main thread ties to join the testing thread when timeout. If the the testing thread is still alive, it is considered blocked, the main thread stops the testing thread and process testing report.
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@198 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/environments/commandline/YetiCLRoutine.java
+++ yeti/environments/commandline/YetiCLRoutine.java
@@ -3,12 +3,19 @@
  */
 package yeti.environments.commandline;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import yeti.YetiCard;
 import yeti.YetiLog;
 import yeti.YetiModule;
 import yeti.YetiName;
 import yeti.YetiRoutine;
 import yeti.YetiType;
+import yeti.environments.commandline.YetiCLInitializer;
+import yeti.environments.commandline.YetiCLRoutine.TestingSession;
 
 /**
  * Class that represents... 
@@ -20,6 +27,8 @@ import yeti.YetiType;
 public class YetiCLRoutine extends YetiRoutine {
 
 	String cmdLine = "";
+	// timeout for each commmand line process
+	int timeout = YetiCLInitializer.timeout;
 	
 	/**
 	 * 
@@ -54,9 +63,11 @@ public class YetiCLRoutine extends YetiRoutine {
 		String cmd = this.cmdLine;
 		
 		for (YetiCard arg0: arg) {
-			cmd = cmd + " " + arg0.toString();
+			//cmd = cmd + " " + arg0.toString();
+			cmd = cmd + " " + arg0.getValue();
 		}
 		
+		/*
 		Process p = Runtime.getRuntime().exec(cmd);
 		int result = p.waitFor();
 		if (result!=0) {
@@ -64,8 +75,75 @@ public class YetiCLRoutine extends YetiRoutine {
 		} else {
 			YetiLog.printYetiLog(cmd, this);
 		}
+		*/
 		
+		//executes the command line process in another thread
+		TestingSession test = new TestingSession(cmd);
+		test.start();
+		test.join(timeout);  // join the thread in timeout million seconds
+		
+		if(test.isAlive()){  // thread still alive when time out
+			test.stop();
+			YetiLog.printYetiThrowable(new Exception("testing time out"), this);
+		}else{				
+			int result = test.getTestResult();  // get the exit value of the command line process
+			if (result!=0) {                    // process exit abnormally
+				YetiLog.printYetiThrowable(new Exception("returned value: "+result), this);
+			} else {
+				YetiLog.printYetiLog(cmd, this);
+			}
+		}
+	
 		return cmd;
 	}
+	
+	/**
+	 * inner class presents a thread in which the command line process is executed
+	 * */
+	class TestingSession extends Thread{
+		
+		String cmd;
+		int result = -1;
+		
+		public TestingSession(String cmd){
+			this.cmd = cmd;
+		}
+		
+		/**
+		 * get the process exit value of the command line
+		 * */
+		public int getTestResult(){
+			return result;
+		}
 
+		public void run(){
+			Process p;
+			try {
+				// execute the command
+				p = Runtime.getRuntime().exec(cmd);
+				
+				// get the output and err stream of the process in case it is blocked by them
+				BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));	
+				BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+				String received = in.readLine();
+				while(received != null){
+					System.out.println(received);
+					received = in.readLine();
+				}
+				received = err.readLine();
+				while(received != null){
+					System.err.println(received);
+					received = err.readLine();
+				}
+				
+				// wait the execution of the process and get the exit value 
+				result = p.waitFor();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
