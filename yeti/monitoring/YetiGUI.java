8ce383c76a5d9f111345f3bdecbd1040d9badd37
moriol <moriol@7f4fa506-1231-44ca-96bd-ce53b71a5133>
2011-04-18 11:56:18 +0000

    Changed tag @overrides
    git-svn-id: https://svn.origo.ethz.ch/yeti/trunk@232 7f4fa506-1231-44ca-96bd-ce53b71a5133

--- yeti/monitoring/YetiGUI.java
+++ yeti/monitoring/YetiGUI.java
@@ -168,21 +168,14 @@ public class YetiGUI implements Runnable {
 		f.add(p,BorderLayout.CENTER);
 		
 		f.addWindowListener(new WindowListener(){
-			@Override
 			public void windowActivated(WindowEvent arg0) {}
-			@Override
 			public void windowClosed(WindowEvent arg0) {}
-			@Override
 			public void windowClosing(WindowEvent arg0) {
 				Yeti.engine.stopTesting();
 			}
-			@Override
 			public void windowDeactivated(WindowEvent arg0) {}
-			@Override
 			public void windowDeiconified(WindowEvent arg0) {}
-			@Override
 			public void windowIconified(WindowEvent arg0) {}
-			@Override
 			public void windowOpened(WindowEvent arg0) {}
 		});
 
